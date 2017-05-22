a43b001aed47f3e171928b53fb0f816146971b16
erikvw <ew2789@gmail.com>
2017-05-22 12:24:38 +0200

    fix signal try/except

--- ambition_subject/models/signals.py
+++ ambition_subject/models/signals.py
@@ -3,18 +3,24 @@ from django.dispatch import receiver
 
 from ambition_screening.models import SubjectScreening
 
-from .subject_consent import SubjectConsent
 from .enrollment import Enrollment
+from .subject_consent import SubjectConsent
 
 
-@receiver(post_save, weak=False, sender=SubjectConsent, dispatch_uid='subject_consent_on_post_save')
+@receiver(post_save, weak=False, sender=SubjectConsent,
+          dispatch_uid='subject_consent_on_post_save')
 def subject_consent_on_post_save(sender, instance, raw, created, using, **kwargs):
+    """Creates an enrollment instance for this consented subject, if
+    it does not exist.
+    """
     if not raw:
         if created:
             try:
+                Enrollment.objects.get(
+                    subject_identifier=instance.subject_identifier,
+                    visit_schedule_name=Enrollment._meta.visit_schedule_name)
+            except Enrollment.DoesNotExist:
                 Enrollment.objects.create(
                     subject_identifier=instance.subject_identifier,
                     consent_identifier=instance.id,
                     is_eligible=instance.subject_screening.eligible)
-            except SubjectScreening.DoesNotExist:
-                raise('Valid subject screening id not found')
