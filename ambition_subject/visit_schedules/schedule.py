cdce6d30cad949e2dd4a5624026835197d6fcb71
erikvw <ew2789@gmail.com>
2017-05-29 09:23:55 +0200

    upgrade visit schedule

--- ambition_subject/visit_schedules/schedule.py
+++ ambition_subject/visit_schedules/schedule.py
@@ -1,5 +1,5 @@
-from edc_visit_schedule.constants import DAYS, WEEKS
-from edc_visit_schedule.schedule import Schedule
+from dateutil.relativedelta import relativedelta
+from edc_visit_schedule import Schedule, Visit
 
 from .crfs import (
     crfs_d5, crfs_d1, crfs_d3, crfs_d7, crfs_d10, crfs_d12, crfs_d14,
@@ -7,103 +7,131 @@ from .crfs import (
 from .requisitions import requisitions, requisitions_d1, requisitions_d3, requisitions_d7
 
 # schedule for new participants
-schedule1 = Schedule(name='schedule1', title='Ambition')
+schedule1 = Schedule(
+    name='schedule1',
+    title='Ambition',
+    enrollment_model='ambition_subject.enrollment',
+    disenrollment_model='ambition_subject.disenrollment',
+)
 
-schedule1.add_visit(
+visit0 = Visit(
     code='1000',
     title='Day 1',
     timepoint=0,
-    base_interval=0,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=0),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d1,
     crfs=crfs_d1)
 
-schedule1.add_visit(
+visit1 = Visit(
     code='1003',
     title='Day 3',
     timepoint=1,
-    base_interval=3,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=3),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d3,
     crfs=crfs_d3)
 
-schedule1.add_visit(
+visit2 = Visit(
     code='1005',
     title='Day 5',
     timepoint=2,
-    base_interval=5,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=5),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d3,
     crfs=crfs_d5)
 
-schedule1.add_visit(
+visit3 = Visit(
     code='1007',
     title='Day 7',
     timepoint=3,
-    base_interval=7,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=7),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d7,
     crfs=crfs_d7)
 
-schedule1.add_visit(
+visit4 = Visit(
     code='1010',
     title='Day 10',
     timepoint=4,
-    base_interval=9,  # TODO: Base interval should be 10
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=10),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d3,
     crfs=crfs_d10)
 
-schedule1.add_visit(
+visit5 = Visit(
     code='1012',
     title='Day 12',
     timepoint=5,
-    base_interval=12,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=12),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d3,
     crfs=crfs_d12)
 
-schedule1.add_visit(
+visit6 = Visit(
     code='1014',
     title='Day 14',
     timepoint=6,
-    base_interval=14,
-    base_interval_unit=DAYS,
+    rbase=relativedelta(days=14),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions_d7,
     crfs=crfs_d14)
 
-schedule1.add_visit(
+visit7 = Visit(
     code='1028',
     title='Week 4',
     timepoint=7,
-    base_interval=4,
-    base_interval_unit=WEEKS,
+    rbase=relativedelta(weeks=4),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions,
     crfs=crfs_w4)
 
-schedule1.add_visit(
+visit8 = Visit(
     code='1042',
     title='Week 6',
     timepoint=8,
-    base_interval=6,
-    base_interval_unit=WEEKS,
+    rbase=relativedelta(weeks=6),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions,
     crfs=crfs_w6)
 
-schedule1.add_visit(
+visit9 = Visit(
     code='1056',
     title='Week 8',
     timepoint=9,
-    base_interval=8,
-    base_interval_unit=WEEKS,
+    rbase=relativedelta(weeks=8),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions,
     crfs=crfs_w8)
 
-schedule1.add_visit(
+visit10 = Visit(
     code='1070',
     title='Week 10',
     timepoint=10,
-    base_interval=10,
-    base_interval_unit=WEEKS,
+    rbase=relativedelta(weeks=10),
+    rlower=relativedelta(days=0),
+    rupper=relativedelta(days=6),
     requisitions=requisitions,
     crfs=crfs_w10)
+
+schedule1.add_visit(visit=visit0)
+schedule1.add_visit(visit=visit1)
+schedule1.add_visit(visit=visit2)
+schedule1.add_visit(visit=visit3)
+schedule1.add_visit(visit=visit4)
+schedule1.add_visit(visit=visit5)
+schedule1.add_visit(visit=visit6)
+schedule1.add_visit(visit=visit7)
+schedule1.add_visit(visit=visit8)
+schedule1.add_visit(visit=visit9)
+schedule1.add_visit(visit=visit10)
--- ambition_subject/visit_schedules/visit_schedule.py
+++ ambition_subject/visit_schedules/visit_schedule.py
@@ -1,17 +1,13 @@
-from edc_visit_schedule.visit_schedule import VisitSchedule
-from edc_visit_schedule.site_visit_schedules import site_visit_schedules
+from edc_visit_schedule import VisitSchedule, site_visit_schedules
 
 from .schedule import schedule1
 
 visit_schedule1 = VisitSchedule(
     name='visit_schedule1',
     verbose_name='Ambition',
-    app_label='ambition_subject',
-    default_enrollment_model='ambition_subject.enrollment',
-    default_disenrollment_model='ambition_subject.disenrollment',
     visit_model='ambition_subject.subjectvisit',
-    offstudy_model='ambition_subject.subjectoffstudy',
-)
+    offstudy_model='ambition_subject.subjectoffstudy')
+
 visit_schedule1.add_schedule(schedule1)
 
 site_visit_schedules.register(visit_schedule1)
