10962819b4f114f40616f8df06a08684e48b32c3
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-06-22 13:28:39 +0200

    fix form ordering

--- ambition_subject/visit_schedules/crfs.py
+++ ambition_subject/visit_schedules/crfs.py
@@ -6,9 +6,9 @@ from edc_visit_schedule.visit import Crf
 # )
 
 crfs_d1 = (
-    Crf(show_order=1, model='ambition_subject.bloodresult'),
-    Crf(show_order=2, model='ambition_subject.patienthistory'),
-    Crf(show_order=3, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=1, model='ambition_subject.patienthistory'),
+    Crf(show_order=2, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=3, model='ambition_subject.bloodresult'),
     Crf(show_order=4, model='ambition_subject.prnmodel'),
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
@@ -20,8 +20,8 @@ crfs_d1 = (
 
 crfs_d3 = (
     Crf(show_order=1, model='ambition_subject.clinicnote'),
-    Crf(show_order=2, model='ambition_subject.bloodresult'),
-    Crf(show_order=3, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=2, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=3, model='ambition_subject.bloodresult'),
     Crf(show_order=4, model='ambition_subject.prnmodel'),
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
@@ -45,8 +45,8 @@ crfs_d5 = (
 
 crfs_d7 = (
     Crf(show_order=1, model='ambition_subject.clinicnote'),
-    Crf(show_order=2, model='ambition_subject.bloodresult'),
-    Crf(show_order=3, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=2, model='ambition_subject.lumbarpuncturecsf'),
+    Crf(show_order=3, model='ambition_subject.bloodresult'),
     Crf(show_order=4, model='ambition_subject.prnmodel'),
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
