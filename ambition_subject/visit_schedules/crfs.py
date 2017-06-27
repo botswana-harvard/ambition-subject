928d36417fc5840aa97cda0de7447c7661b3af6d
Itumeleng Mosweu <imosweu@nkange.bhp.org.bw>
2017-06-27 15:16:41 +0200

    remove recurrence of symptoms day 1 to 14

--- ambition_subject/visit_schedules/crfs.py
+++ ambition_subject/visit_schedules/crfs.py
@@ -13,9 +13,7 @@ crfs_d1 = (
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
     Crf(show_order=7, model='ambition_subject.radiology', required=False),
-    Crf(show_order=8, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=9, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
         required=False),)
 
 crfs_d3 = (
@@ -26,9 +24,7 @@ crfs_d3 = (
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
     Crf(show_order=7, model='ambition_subject.radiology', required=False),
-    Crf(show_order=8, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=9, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
         required=False),)
 
 crfs_d5 = (
@@ -38,9 +34,7 @@ crfs_d5 = (
     Crf(show_order=4, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=5, model='ambition_subject.microbiology', required=False),
     Crf(show_order=6, model='ambition_subject.radiology', required=False),
-    Crf(show_order=7, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=7, model='ambition_subject.protocoldeviationviolation',
         required=False),)
 
 crfs_d7 = (
@@ -51,9 +45,7 @@ crfs_d7 = (
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
     Crf(show_order=7, model='ambition_subject.radiology', required=False),
-    Crf(show_order=8, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=9, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
         required=False),)
 
 crfs_d10 = (
@@ -63,9 +55,7 @@ crfs_d10 = (
     Crf(show_order=4, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=5, model='ambition_subject.microbiology', required=False),
     Crf(show_order=6, model='ambition_subject.radiology', required=False),
-    Crf(show_order=7, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=7, model='ambition_subject.protocoldeviationviolation',
         required=False),)
 
 crfs_d12 = (
@@ -75,9 +65,7 @@ crfs_d12 = (
     Crf(show_order=4, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=5, model='ambition_subject.microbiology', required=False),
     Crf(show_order=6, model='ambition_subject.radiology', required=False),
-    Crf(show_order=7, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=7, model='ambition_subject.protocoldeviationviolation',
         required=False))
 
 crfs_d14 = (
@@ -88,18 +76,29 @@ crfs_d14 = (
     Crf(show_order=5, model='ambition_subject.adverseevent', required=False),
     Crf(show_order=6, model='ambition_subject.microbiology', required=False),
     Crf(show_order=7, model='ambition_subject.radiology', required=False),
-    Crf(show_order=8, model='ambition_subject.recurrencesymptom',
-        required=False),
-    Crf(show_order=9, model='ambition_subject.protocoldeviationviolation',
+    Crf(show_order=8, model='ambition_subject.protocoldeviationviolation',
         required=False))
 
 crfs_w4 = (
     Crf(show_order=1, model='ambition_subject.week4'),
-    Crf(show_order=2, model='ambition_subject.bloodresult'))
+    Crf(show_order=2, model='ambition_subject.bloodresult'),
+    Crf(show_order=3, model='ambition_subject.recurrencesymptom',
+        required=False)
+)
 
-crfs_w6 = (Crf(show_order=1, model='ambition_subject.followup'),)
+crfs_w6 = (Crf(show_order=1, model='ambition_subject.followup'),
+           Crf(show_order=8, model='ambition_subject.recurrencesymptom',
+               required=False)
+           )
 
-crfs_w8 = (Crf(show_order=1, model='ambition_subject.followup'),)
+crfs_w8 = (Crf(show_order=1, model='ambition_subject.followup'),
+           Crf(show_order=8, model='ambition_subject.recurrencesymptom',
+               required=False)
+           )
 
 crfs_w10 = (Crf(show_order=1, model='ambition_subject.followup'),
-            Crf(show_order=2, model='ambition_subject.studyterminationconclusion'),)
+            Crf(show_order=2,
+                model='ambition_subject.studyterminationconclusion'),
+            Crf(show_order=8, model='ambition_subject.recurrencesymptom',
+                required=False)
+            )
