2b82615a178a86c2727806dae38c66863d2607da
Itumeleng Mosweu <imosweuk@gmail.com>
2017-07-07 11:31:40 +0200

    move death form validations

--- ambition_subject/forms/death_report_form.py
+++ ambition_subject/forms/death_report_form.py
@@ -1,3 +1,5 @@
+from ambition_subject_validations.form_validators import DeathFormValidator
+
 from ..models import DeathReport
 
 from .form_mixins import SubjectModelFormMixin
@@ -6,32 +8,10 @@ from .form_mixins import SubjectModelFormMixin
 class DeathReportForm(SubjectModelFormMixin):
 
     def clean(self):
-
-        field_list = ['cause_of_death_study_doctor_opinion',
-                      'cause_of_death_tmg1_opinion',
-                      'cause_of_death_tmg2_opinion']
-        field_required_list = ['cause_tb_study_doctor_opinion',
-                               'cause_tb_tmg1_opinion',
-                               'cause_tb_tmg2_opinion']
-
-        for field, field_required in zip(field_list, field_required_list):
-            self.required_if(
-                'TB',
-                field=field,
-                field_required=field_required)
-
-        field_list_2 = ['cause_of_death_study_doctor_opinion',
-                        'cause_of_death_tmg1_opinion',
-                        'cause_of_death_tmg2_opinion']
-        field_required_list_2 = ['cause_tb_study_doctor_opinion',
-                                 'cause_tb_tmg1_opinion',
-                                 'cause_tb_tmg2_opinion']
-
-        for field, field_required in zip(field_list_2, field_required_list_2):
-            self.required_if(
-                'TB',
-                field=field,
-                field_required=field_required)
+        cleaned_data = super().clean()
+        cleaned_data = DeathFormValidator(
+            cleaned_data=cleaned_data).clean()
+        return cleaned_data
 
     class Meta:
         model = DeathReport
