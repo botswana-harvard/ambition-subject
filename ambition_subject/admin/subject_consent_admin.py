3ce4c69e11514acbd7ed4620b2ec8906f5107411
Itumeleng Mosweu <imosweuk@gmail.com>
2017-07-26 14:16:03 +0200

    update subject identifier on saving consent

--- ambition_subject/admin/subject_consent_admin.py
+++ ambition_subject/admin/subject_consent_admin.py
@@ -6,7 +6,7 @@ from django_revision.modeladmin_mixin import ModelAdminRevisionMixin
 from ambition_screening.models import SubjectScreening
 from edc_base.modeladmin_mixins import (
     ModelAdminInstitutionMixin, audit_fieldset_tuple, audit_fields,
-    ModelAdminNextUrlRedirectMixin)
+    ModelAdminNextUrlRedirectMixin, ModelAdminNextUrlRedirectError)
 from edc_consent.modeladmin_mixins import ModelAdminConsentMixin
 
 
@@ -15,9 +15,31 @@ from ..forms import SubjectConsentForm
 from ..models import SubjectConsent
 
 
+class ModelAdminMixin(ModelAdminNextUrlRedirectMixin, ModelAdminRevisionMixin,
+                      ModelAdminInstitutionMixin):
+
+    list_per_page = 10
+    date_hierarchy = 'modified'
+    empty_value_display = '-'
+
+    def redirect_url(self, request, obj, post_url_continue=None):
+        redirect_url = super().redirect_url(
+            request, obj, post_url_continue=post_url_continue)
+        if request.GET.dict().get('next'):
+            url_name = request.GET.dict().get('next').split(',')[0]
+            attrs = request.GET.dict().get('next').split(',')[1:]
+            options = {k: request.GET.dict().get(k)
+                       for k in attrs if request.GET.dict().get(k)}
+            options.update(subject_identifier=obj.subject_identifier)
+            try:
+                redirect_url = reverse(url_name, kwargs=options)
+            except NoReverseMatch as e:
+                raise ModelAdminNextUrlRedirectError(f'{e}. Got url_name={url_name}, kwargs={options}.')
+        return redirect_url
+
+
 @admin.register(SubjectConsent, site=ambition_subject_admin)
-class SubjectConsentAdmin(ModelAdminConsentMixin, ModelAdminRevisionMixin,
-                          ModelAdminInstitutionMixin, ModelAdminNextUrlRedirectMixin,
+class SubjectConsentAdmin(ModelAdminConsentMixin, ModelAdminMixin,
                           admin.ModelAdmin):
 
     form = SubjectConsentForm
