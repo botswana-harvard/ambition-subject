189a00a2c7f55b1c11cd5c1503516a06d2d47692
erikvw <ew2789@gmail.com>
2017-08-15 12:29:07 +0200

    fix appointment form

--- ambition_subject/forms/appointment_form.py
+++ ambition_subject/forms/appointment_form.py
@@ -8,11 +8,6 @@ from ..choices import APPOINTMENT_REASON, AMB_APPT_TYPE
 
 class AppointmentForm(AppointmentFormMixin, forms.ModelForm):
 
-    appt_type = forms.ChoiceField(
-        label='Appointment type',
-        choices=AMB_APPT_TYPE,
-        widget=forms.RadioSelect)
-
     appt_reason = forms.ChoiceField(
         label='Reason for appointment:',
         choices=APPOINTMENT_REASON,
--- ambition_subject/models/appointment.py
+++ ambition_subject/models/appointment.py
@@ -1,10 +1,25 @@
+from django.db import models
 from edc_appointment.managers import AppointmentManager
 from edc_appointment.model_mixins import AppointmentModelMixin
 from edc_base.model_managers import HistoricalRecords
 from edc_base.model_mixins import BaseUuidModel
+from ambition_subject.choices import AMB_APPT_TYPE, APPOINTMENT_REASON
 
 
 class Appointment(AppointmentModelMixin, BaseUuidModel):
+
+    appt_type = models.CharField(
+        verbose_name='Appointment type',
+        choices=AMB_APPT_TYPE,
+        default='clinic',
+        max_length=20,
+        help_text=(
+            'Default for subject may be edited Subject Configuration.'))
+
+    appt_reason = models.CharField(
+        verbose_name='Reason for appointment:',
+        max_length=25,
+        choices=APPOINTMENT_REASON)
 
     objects = AppointmentManager()
 
