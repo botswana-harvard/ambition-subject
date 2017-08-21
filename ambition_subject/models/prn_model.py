6f6e547192438ac26364ffddc8dae079ccb5d05b
Itumeleng Mosweu <imosweuk@gmail.com>
2017-08-21 12:14:30 +0200

    default prn model field values

--- ambition_subject/models/prn_model.py
+++ ambition_subject/models/prn_model.py
@@ -10,37 +10,43 @@ class PrnModel(CrfModelMixin):
     adverse_event = models.CharField(
         verbose_name='Adverse event?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     adverse_event_tmg = models.CharField(
         verbose_name='Adverse event TMG?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     adverse_event_followup = models.CharField(
         verbose_name='Adverse event follow up?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     blood_result = models.CharField(
         verbose_name='Blood result?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     microbiology = models.CharField(
         verbose_name='Microbiology?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     radiology = models.CharField(
         verbose_name='Radiology?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     recurrence_symptom = models.CharField(
@@ -54,30 +60,35 @@ class PrnModel(CrfModelMixin):
     protocol_deviation = models.CharField(
         verbose_name='Protocol Deviation?',
         max_length=5,
-        choices=YES_NO
+        choices=YES_NO,
+        default=NO
     )
 
     lumbar_puncture = models.CharField(
         verbose_name='Lumbar puncture?',
         max_length=5,
         choices=YES_NO,
+        default=NO,
         null=True
     )
 
     death_report = models.CharField(
         verbose_name='Death Report?',
         max_length=5,
-        choices=YES_NO)
+        choices=YES_NO,
+        default=NO)
 
     death_report_tmg1 = models.CharField(
         verbose_name='Death Report TMG1?',
         max_length=5,
-        choices=YES_NO)
+        choices=YES_NO,
+        default=NO)
 
     death_report_tmg2 = models.CharField(
         verbose_name='Death Report TMG2?',
         max_length=5,
-        choices=YES_NO)
+        choices=YES_NO,
+        default=NO)
 
     class Meta(CrfModelMixin.Meta):
         verbose_name = 'PRN Form'
