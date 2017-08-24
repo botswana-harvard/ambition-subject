90edf6d42200458c19d99243fa5a416336935e16
Itumeleng Mosweu <imosweuk@gmail.com>
2017-08-24 12:31:47 +0200

    fix subject requisition form

--- ambition_subject/forms/subject_requisition_form.py
+++ ambition_subject/forms/subject_requisition_form.py
@@ -6,10 +6,6 @@ from .form_mixins import SubjectModelFormMixin
 
 class SubjectRequisitionForm(RequisitionFormMixin, SubjectModelFormMixin):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['panel_name'].initial = self._meta.model.panel_name[0:21]
-
     class Meta:
         model = SubjectRequisition
         fields = '__all__'
