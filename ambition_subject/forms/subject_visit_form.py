561561b7148bd919edcea86822763848c9fbf4dc
Leslie <leslie.kep@gmail.com>
2017-09-11 13:11:26 +0200

    remove validator

--- ambition_subject/forms/subject_visit_form.py
+++ ambition_subject/forms/subject_visit_form.py
@@ -3,12 +3,12 @@ from django import forms
 from edc_base.modelform_mixins import CommonCleanModelFormMixin
 from edc_base.modelform_validators import FormValidatorMixin
 from edc_consent.modelform_mixins import RequiresConsentModelFormMixin
-from edc_visit_tracking.form_validators import VisitFormValidator
+
 
 from ..models import SubjectVisit
 
 
-class SubjectVisitForm (FormValidatorMixin, VisitFormValidator, RequiresConsentModelFormMixin,
+class SubjectVisitForm (FormValidatorMixin, RequiresConsentModelFormMixin,
                         CommonCleanModelFormMixin, forms.ModelForm):
 
     form_validator_cls = SubjectVisitFormValidator
