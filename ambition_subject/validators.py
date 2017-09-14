f8938c10ed5ea5e38d2167d93c02d96b498ce861
Itumeleng Mosweu <imosweuk@gmail.com>
2017-09-14 17:32:36 +0200

    update subject validators

--- ambition_subject/validators.py
+++ ambition_subject/validators.py
@@ -5,9 +5,10 @@ bp_validator = RegexValidator(
     '^\d{1,3}\/\d{1,3}$', message='Enter a valid BP in SYS/DIA format')
 
 hm_validator = RegexValidator(
-    '^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$',
+    '^([0-9]{1,3}:[0-5][0-9])$',
     message='Enter a valid time in hour:minutes format')
 
 span_validator = RegexValidator(
-    '^\d{1}\/\d{1,2}\/\d{1,2}$',
+    '^(([1-9]day|([1-2][0-9]|3[0-1])days)|\/|([1-9]week|([1-4][0-9]|5[0-2])weeks)'
+    '|\/|([1-9]month|1[0-2]months))$',
     message='Enter a valid duration in days/weeks/months')
