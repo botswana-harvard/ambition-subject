ac32c90b6cb22ebb7fbfc7ea2a9223686919c607
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-10-20 11:20:15 +0200

    link pharma to ambition

--- ambition_subject/models/signals.py
+++ ambition_subject/models/signals.py
@@ -1,6 +1,11 @@
+from ambition_rando.models import SubjectRandomization
 from ambition_rando.randomizer import Randomizer
+from ambition_subject.constants import CONTROL, SINGLE_DOSE
+from ambition_subject.models.patient_history import PatientHistory
 from edc_base.utils import get_utcnow
-from edc_pharma.scheduler import DispensePlanScheduler
+from edc_pharma import DispenseAppointmentDescribe
+from edc_pharma.dispense import PrescriptionCreator
+from edc_pharma.scheduler import DispenseScheduler
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -29,9 +34,32 @@ def subject_consent_on_post_save(sender, instance, raw, created, **kwargs):
                     subject_identifier=instance.subject_identifier,
                     consent_identifier=instance.consent_identifier,
                     is_eligible=subject_screening.eligible)
-            obj = Randomizer(subject_consent=instance,
-                             randomization_datetime=get_utcnow())
+            Randomizer(subject_consent=instance,
+                       randomization_datetime=get_utcnow())
             subject_screening.subject_identifier = instance.subject_identifier
             subject_screening.save_base(update_fields=['subject_identifier'])
-            #DispensePlanScheduler(
-            #    obj, arm=obj.rx).create_schedules()
+
+
+@receiver(post_save, weak=False, sender=PatientHistory,
+          dispatch_uid='patient_history_on_post_save')
+def patient_history_on_post_save(sender, instance, raw, created, **kwargs):
+    """Creates an enrollment instance for this consented subject, if
+    it does not exist.
+    """
+    if not raw:
+        if created:
+            subject_randomization = SubjectRandomization.objects.get(
+                subject_identifier=instance.subject_identifier)
+            if subject_randomization.rx in [CONTROL, SINGLE_DOSE]:
+                scheduler = DispenseScheduler(
+                    subject_identifier=subject_randomization.subject_identifier,
+                    randomization_datetime=subject_randomization.randomization_datetime,
+                    arm=subject_randomization.rx
+                )
+                options = dict(weight=instance.weight)
+                for appointment in scheduler.dispense_appointments:
+                    describer = DispenseAppointmentDescribe(
+                        dispense_appointment=appointment)
+                    options.update({'duration': describer.duration})
+                    PrescriptionCreator(
+                        dispense_appointment=appointment, options=options)
--- ambition_subject/tests/test_subject_dispenses_schedule.py
+++ ambition_subject/tests/test_subject_dispenses_schedule.py
@@ -1,13 +1,15 @@
 from ambition_rando.import_randomization_list import import_randomization_list
 from edc_base.utils import get_utcnow
-from edc_pharma.models.dispense_schedule import DispenseSchedule
-from edc_pharma.models.dispense_timepoint import DispenseTimepoint
+from edc_pharma.models import DispenseAppointment
+from edc_pharma.models import DispenseSchedule
+from edc_pharma.models import Prescription
 from edc_visit_tracking.constants import SCHEDULED
 
 from django.test import TestCase, tag
 from model_mommy import mommy
 
 from ..models import Appointment
+from ..models import SubjectVisit
 
 
 class TestSubjectDispenseSchedule(TestCase):
@@ -21,15 +23,22 @@ class TestSubjectDispenseSchedule(TestCase):
             consent_datetime=get_utcnow(),
             screening_identifier=screening.screening_identifier)
 
-        self.visit_code = '1070'
+        self.visit_code = '1000'
+        self.appointment = Appointment.objects.all().order_by('timepoint').first()
+        self.subject_visit = mommy.make_recipe(
+            'ambition_subject.subjectvisit',
+            appointment=self.appointment,
+            reason=SCHEDULED)
 
-        for appointment in Appointment.objects.all().order_by('timepoint'):
-            self.subject_visit = mommy.make_recipe(
-                'ambition_subject.subjectvisit',
-                appointment=appointment,
-                reason=SCHEDULED)
-            if appointment.visit_code == self.visit_code:
-                break
+    @tag('pre')
+    def test_create_prescriptions_by_patienthistory(self):
+        visit = SubjectVisit.objects.get(appointment=self.appointment)
+        mommy.make_recipe(
+            'ambition_subject.patienthistory',
+            subject_visit=visit)
+        self.assertEqual(DispenseSchedule.objects.filter(
+            subject_identifier=self.consent.subject_identifier).count(), 2)
+        self.assertEqual(Prescription.objects.all().count(), 9)
 
     def test_single_dose_schedule(self):
         """ Assert that dispense timepoints for single dose subject
@@ -37,10 +46,10 @@ class TestSubjectDispenseSchedule(TestCase):
         """
         self.assertEqual(DispenseSchedule.objects.filter(
             subject_identifier=self.consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule1',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule2',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
 
@@ -52,10 +61,10 @@ class TestSubjectDispenseSchedule(TestCase):
         self.consent.save()
         self.assertEqual(DispenseSchedule.objects.filter(
             subject_identifier=self.consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule1',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule2',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
 
@@ -74,9 +83,9 @@ class TestSubjectDispenseSchedule(TestCase):
 
         self.assertEqual(DispenseSchedule.objects.filter(
             subject_identifier=consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule1',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
-        self.assertEqual(DispenseTimepoint.objects.filter(
+        self.assertEqual(DispenseAppointment.objects.filter(
             schedule__name='schedule2',
             schedule__subject_identifier=self.consent.subject_identifier).count(), 2)
