ec432ec2e3c4e4b2dd46948023cd7a5c55c56a7d
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2017-11-10 16:04:55 +0200

    subject rules functional testing

--- ambition_subject/models/subject_offstudy.py
+++ ambition_subject/models/subject_offstudy.py
@@ -1,9 +1,10 @@
-from edc_base.model_mixins import BaseUuidModel
+from ambition_subject.models.model_mixins.crf_model_mixin import CrfModelMixin
+
 from edc_base.model_managers import HistoricalRecords
 from edc_offstudy.model_mixins import OffstudyModelMixin, OffstudyModelManager
 
 
-class SubjectOffstudy(OffstudyModelMixin, BaseUuidModel):
+class SubjectOffstudy(OffstudyModelMixin, CrfModelMixin):
 
     """A model completed by the user that completed when the
     subject is taken off-study.
