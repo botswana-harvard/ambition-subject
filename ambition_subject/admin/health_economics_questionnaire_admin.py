9213d6bfad8f276dada1b9fedd7a2a32d8516b87
Itumeleng Mosweu <imosweuk@gmail.com>
2017-11-14 08:22:41 +0200

    update health economics questionnaire form

--- ambition_subject/admin/health_economics_questionnaire_admin.py
+++ ambition_subject/admin/health_economics_questionnaire_admin.py
@@ -57,38 +57,15 @@ class HealthEconomicsQuestionnaireAdmin(CrfModelAdminMixin, admin.ModelAdmin):
                 'personal_he_spend',
                 'proxy_he_spend',
                 'he_spend_last_4weeks',
-                'care_before_hospital',
                 'duration_present_condition',
                 'activities_missed',
                 'activities_missed_other',
                 'time_off_work',
                 'carer_time_off',
                 'loss_of_earnings',
-                'earnings_lost_amount'
+                'earnings_lost_amount',
+                'care_before_hospital'
             ]}
-         ),
-
-        ('Educational Background', {
-            'fields': [
-                'household_head',
-                'profession',
-                'education_years',
-                'education_certificate',
-                'elementary_school',
-                'elementary_attendance_years',
-                'secondary_school',
-                'secondary_attendance_years',
-                'higher_education',
-                'higher_attendance_years',
-                'head_profession',
-                'head_education_years',
-                'head_education_certificate',
-                'head_elementary',
-                'head_attendance_years',
-                'head_secondary',
-                'head_secondary_years',
-                'head_higher_education',
-                'head_higher_years']}
          ), audit_fieldset_tuple)
 
     radio_fields = {
@@ -96,15 +73,8 @@ class HealthEconomicsQuestionnaireAdmin(CrfModelAdminMixin, admin.ModelAdmin):
         'care_before_hospital': admin.VERTICAL,
         'care_provider': admin.VERTICAL,
         'currency': admin.VERTICAL,
-        'elementary_school': admin.VERTICAL,
-        'higher_education': admin.VERTICAL,
-        'head_elementary': admin.VERTICAL,
-        'head_secondary': admin.VERTICAL,
-        'household_head': admin.VERTICAL,
-        'head_higher_education': admin.VERTICAL,
         'location_care': admin.VERTICAL,
         'medication_bought': admin.VERTICAL,
         'other_place_visited': admin.VERTICAL,
-        'secondary_school': admin.VERTICAL,
         'transport_form': admin.VERTICAL,
         'loss_of_earnings': admin.VERTICAL}
--- ambition_subject/forms/health_economics_questionnaire_form.py
+++ ambition_subject/forms/health_economics_questionnaire_form.py
@@ -1,6 +1,6 @@
 from ..models import HealthEconomicsQuestionnaire
 from .form_mixins import SubjectModelFormMixin
-# from ambition_validators import PatientHistoryFormValidator
+from ambition_validators import HealthEconomicsQuestionnaireFormValidator
 from django.forms import forms
 from edc_base.modelform_validators.base_form_validator import NOT_REQUIRED_ERROR
 from edc_constants.constants import NOT_APPLICABLE, YES
@@ -8,17 +8,33 @@ from edc_constants.constants import NOT_APPLICABLE, YES
 
 class HealthEconomicsQuestionnaireForm(SubjectModelFormMixin):
 
-    #     form_validator_cls = PatientHistoryFormValidator
-
     def clean(self):
-        if (self.data.get('care_before_hospital') == YES
-                and self.data.get('medicalexpenses_set-0-location_care') is NOT_APPLICABLE):
+        HealthEconomicsQuestionnaireFormValidator(
+            cleaned_data=self.cleaned_data).clean()
 
-            message = 'This field is applicable.'
+        if (self.data.get('care_before_hospital') == YES
+                and self.data.get(
+                    'medicalexpenses_set-0-location_care') == NOT_APPLICABLE):
+
+            message = 'The medical expenses inline is required.'
             raise forms.ValidationError(message, code=NOT_REQUIRED_ERROR)
 
         if self.data.get('medicalexpenses_set-3-location_care'):
             message = 'Exceeded maximum number of medical expenses inlines.'
+            raise forms.ValidationError(message, code=NOT_REQUIRED_ERROR)
+
+        if (self.data.get('medicalexpenses_set-0-other_place_visited') == YES
+                and self.data.get(
+                    'medicalexpenses_set-1-location_care') == NOT_APPLICABLE):
+
+            message = 'Please fill in the second medical expenses inline.'
+            raise forms.ValidationError(message, code=NOT_REQUIRED_ERROR)
+
+        if (self.data.get('medicalexpenses_set-1-other_place_visited') == YES
+                and self.data.get(
+                    'medicalexpenses_set-2-location_care') == NOT_APPLICABLE):
+
+            message = 'Please fill in the third medical expenses inline.'
             raise forms.ValidationError(message, code=NOT_REQUIRED_ERROR)
 
     class Meta:
