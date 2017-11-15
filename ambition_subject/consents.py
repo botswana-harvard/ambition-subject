d187d987db090c5d3621a16a8dc7526d013eae54
erikvw <ew2789@gmail.com>
2017-11-15 22:44:18 +0200

    get study start/end dates from edc_protocol

--- ambition_subject/consents.py
+++ ambition_subject/consents.py
@@ -1,23 +1,15 @@
-import arrow
-
-from datetime import datetime
-from dateutil.tz import gettz
 from django.apps import apps as django_apps
 from edc_consent.consent import Consent
 from edc_consent.site_consents import site_consents
 from edc_constants.constants import MALE, FEMALE
 
-app_config = django_apps.get_app_config('edc_protocol')
-
-tzinfo = gettz('Africa/Gaborone')
+edc_protocol = django_apps.get_app_config('edc_protocol')
 
 v1 = Consent(
     'ambition_subject.subjectconsent',
     version='1',
-    start=arrow.get(
-        datetime(2016, 12, 31, 0, 0, 0), tzinfo=tzinfo).to('UTC').datetime,
-    end=arrow.get(
-        datetime(2018, 12, 31, 23, 59, 59), tzinfo=tzinfo).to('UTC').datetime,
+    start=edc_protocol.study_open_datetime,
+    end=edc_protocol.study_close_datetime,
     age_min=18,
     age_is_adult=18,
     age_max=64,
