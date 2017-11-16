2e662bf070544b79fb8ae774bd72554fb5f69f95
erikvw <ew2789@gmail.com>
2017-11-16 21:06:00 +0200

    move eligibility logic off of model

--- ambition_subject/eligibility/__init__.py
+++ ambition_subject/eligibility/__init__.py
@@ -2,3 +2,4 @@ from .age_evaluator import AgeEvaluator
 from .early_withdrawal_evaluator import EarlyWithdrawalEvaluator
 from .eligibility import Eligibility, EligibilityError
 from .gender_evaluator import GenderEvaluator
+from .subject_screening_eligibility import SubjectScreeningEligibility
