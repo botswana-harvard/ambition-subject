3bd5e6bc834aac3504a9ad69af223c3c1e756e16
erikvw <ew2789@gmail.com>
2017-11-20 15:26:16 +0200

    fix site url

--- ambition_subject/admin_site.py
+++ ambition_subject/admin_site.py
@@ -5,7 +5,7 @@ class AdminSite(DjangoAdminSite):
     site_title = 'Ambition Subject'
     site_header = 'Ambition Subject'
     index_title = 'Ambition Subject'
-    site_url = '/admininistration/'
+    site_url = '/administration/'
 
 
 ambition_subject_admin = AdminSite(name='ambition_subject_admin')
