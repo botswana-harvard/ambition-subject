664d1c6ae07d61bb57befe5e2abee970220eec97
Leslie <leslie.kep@gmail.com>
2017-11-27 14:47:35 +0200

    update unknown variable

--- ambition_subject/tests/test_early_withdrawal_evaluator.py
+++ ambition_subject/tests/test_early_withdrawal_evaluator.py
@@ -7,16 +7,16 @@ from ..eligibility import EarlyWithdrawalEvaluator
 class TestEarlyWithdrawalEvaluator(TestCase):
 
     def test_early_withdrawal_criteria_no(self):
-        opts = dict(alt=None, pmns=None, platlets=None)
+        opts = dict(alt=None, pmn=None, platlets=None)
         obj = EarlyWithdrawalEvaluator(**opts)
         self.assertFalse(obj.eligible)
 
     def test_early_withdrawal_criteria_with_none(self):
-        opts = dict(alt=None, pmns=None, platlets=None, allow_none=True)
+        opts = dict(alt=None, pmn=None, platlets=None, allow_none=True)
         obj = EarlyWithdrawalEvaluator(**opts)
         self.assertTrue(obj.eligible)
 
     def test_early_withdrawal_criteria_ok(self):
-        opts = dict(alt=200, pmns=500, platlets=50, allow_none=True)
+        opts = dict(alt=200, pmn=500, platlets=50, allow_none=True)
         obj = EarlyWithdrawalEvaluator(**opts)
         self.assertTrue(obj.eligible)
