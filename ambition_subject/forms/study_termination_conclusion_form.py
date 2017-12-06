3e8c8d72e9c673ea593517dfc6d3e2118e05d5a7
Itumeleng Mosweu <imosweuk@gmail.com>
2017-12-06 11:39:40 +0200

    fix study termination form

--- ambition_subject/forms/study_termination_conclusion_form.py
+++ ambition_subject/forms/study_termination_conclusion_form.py
@@ -1,13 +1,24 @@
+from django.apps import apps as django_apps
 from django import forms
 from ambition_validators import StudyTerminationConclusionFormValidator
 
-from ..models import StudyTerminationConclusion
+from ..models import PatientHistory, StudyTerminationConclusion
 from .form_mixins import SubjectModelFormMixin
 
 
 class StudyTerminationConclusionForm(SubjectModelFormMixin):
 
-    form_validator_cls = StudyTerminationConclusionFormValidator
+    def clean(self):
+        cleaned_data = super().clean()
+        try:
+            form_validator = StudyTerminationConclusionFormValidator(
+                cleaned_data=cleaned_data, instance=self.instance,
+                patient_history_cls=django_apps.get_model(PatientHistory._meta.label_lower))
+        except TypeError:
+            pass
+        else:
+            cleaned_data = form_validator.validate()
+        return cleaned_data
 
     action_identifier = forms.CharField(
         label='Action Identifier',
