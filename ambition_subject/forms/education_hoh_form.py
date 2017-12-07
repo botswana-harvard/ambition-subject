d1634978435ac65148670e20b07022b35680c676
aseokana <aseokana@bhp.org.bw>
2017-12-07 14:46:04 +0200

    changing field labels

--- ambition_subject/forms/education_hoh_form.py
+++ ambition_subject/forms/education_hoh_form.py
@@ -8,6 +8,18 @@ class EducationHohForm(SubjectModelFormMixin):
 
     form_validator_cls = EducationFormValidator
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['profession'].label = ('What is their profession?')
+        self.fields['education_certificate'].label = (
+            'What is the their highest education certificate?')
+        self.fields['elementary'].label = (
+            'Did they go to elementary/primary school?')
+        self.fields['secondary'].label = (
+            'Did they go to secondary school?')
+        self.fields['higher_education'].label = (
+            'Did they go to higher education?')
+
     class Meta:
         model = EducationHoh
         fields = '__all__'
