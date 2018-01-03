8889ea5fc94ff1541aa6dcb9e1416e842492c1e4
erikvw <ew2789@gmail.com>
2018-01-03 14:06:27 +0200

    remove requiresconsent form mixin

--- ambition_subject/forms/subject_visit_form.py
+++ ambition_subject/forms/subject_visit_form.py
@@ -1,14 +1,12 @@
 from ambition_validators import SubjectVisitFormValidator
 from django import forms
-from edc_consent.modelform_mixins import RequiresConsentModelFormMixin
 from edc_form_validators import FormValidatorMixin
 
 
 from ..models import SubjectVisit
 
 
-class SubjectVisitForm (FormValidatorMixin, RequiresConsentModelFormMixin,
-                        forms.ModelForm):
+class SubjectVisitForm (FormValidatorMixin, forms.ModelForm):
 
     form_validator_cls = SubjectVisitFormValidator
 
