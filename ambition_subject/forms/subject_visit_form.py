fd80057f7ab00dd7e611031ae1a43e16c9cb156e
erikvw <ew2789@gmail.com>
2018-01-09 11:11:41 +0200

    use base class validator for subject visit form

--- ambition_subject/forms/subject_visit_form.py
+++ ambition_subject/forms/subject_visit_form.py
@@ -1,14 +1,13 @@
-from ambition_validators import SubjectVisitFormValidator
 from django import forms
 from edc_form_validators import FormValidatorMixin
-
+from edc_visit_tracking.form_validators import VisitFormValidator
 
 from ..models import SubjectVisit
 
 
 class SubjectVisitForm (FormValidatorMixin, forms.ModelForm):
 
-    form_validator_cls = SubjectVisitFormValidator
+    form_validator_cls = VisitFormValidator
 
     class Meta:
         model = SubjectVisit
--- ambition_subject/settings.py
+++ ambition_subject/settings.py
@@ -73,6 +73,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'edc_dashboard.middleware.DashboardMiddleware',
+    'edc_subject_dashboard.middleware.DashboardMiddleware',
 ]
 
 ROOT_URLCONF = 'ambition_subject.urls'
