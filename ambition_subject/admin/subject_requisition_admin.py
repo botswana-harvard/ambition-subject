78a2e960f747725df7430dfabdfc63cdb6a41631
erikvw <ew2789@gmail.com>
2018-01-13 09:50:39 +0200

    filter results for autocomplete for lumbarpuncturecsf

--- ambition_subject/admin/subject_requisition_admin.py
+++ ambition_subject/admin/subject_requisition_admin.py
@@ -52,7 +52,7 @@ class SubjectRequisitionAdmin(CrfModelAdminMixin,
         queryset, use_distinct = super().get_search_results(request, queryset, search_term)
         path = urlsplit(request.META.get('HTTP_REFERER')).path
         query = urlsplit(request.META.get('HTTP_REFERER')).query
-        if 'bloodresult' in path:
+        if 'bloodresult' in path or 'lumbarpuncturecsf' in path:
             attrs = parse_qs(query)
             try:
                 subject_visit = attrs.get('subject_visit')[0]
