b33fbbb500c17835e13d86b519388a00b5a949d8
erikvw <ew2789@gmail.com>
2018-01-15 02:50:42 +0200

    undo

--- ambition_subject/models/subject_requisition.py
+++ ambition_subject/models/subject_requisition.py
@@ -43,8 +43,6 @@ class SubjectRequisition(
         default=NOT_APPLICABLE,
         choices=REASON_NOT_DRAWN)
 
-    panel_name = models.CharField(max_length=50, null=True)
-
     objects = Manager()
 
     history = HistoricalRecords()
