818ec194899f69cf1725d00724054af93741065c
erikvw <ew2789@gmail.com>
2018-01-16 19:11:20 +0200

    remove unused models

--- ambition_subject/forms/clinic_note_form.py
+++
@@ -1,9 +0,0 @@
-from .form_mixins import SubjectModelFormMixin
-from ..models import ClinicNote
-
-
-class ClinicNoteForm(SubjectModelFormMixin):
-
-    class Meta:
-        model = ClinicNote
-        fields = '__all__'
